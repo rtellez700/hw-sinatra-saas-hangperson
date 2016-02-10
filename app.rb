6f94302108c77708c112b0bc54fb7f1276d91f3e
Rodrigo Tellez <rtellez700@gmail.com>
2016-02-10 01:56:39 +0000

    Fix route for /create in new.erb

--- app.rb
+++ app.rb
@@ -40,6 +40,9 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    # @game.guess
+    
+        
     redirect '/show'
   end
   
